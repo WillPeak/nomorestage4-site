5afbb3ff8ef79e4c790f69ad4804a4eb61a9ccf4
WillPeak <williamjpeak@gmail.com>
2026-08-23 08:56:42 -0400

    Update JS loader for 62 parts

--- assets/index-B397L99P.js
+++ assets/index-B397L99P.js
@@ -1,4 +1,4 @@
-const n=41;
+const n=62;
 const parts=await Promise.all(Array.from({length:n},(_,i)=>fetch(new URL(`./index-B397L99P.${i}.part.js`,import.meta.url)).then(r=>{if(!r.ok)throw new Error(String(r.status));return r.text()})));
 const s=document.createElement("script");
 s.textContent=parts.join("");
